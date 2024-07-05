83742ac29e3f6d5acd15673d995a5b2629030bc0
Jesus Omar López González <jesusomar-dev@hotmail.com>
2024-07-05 13:00:57 -0600

    dv-Omar: Solution Server

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string;
   readonly SUPABASE_ANON_KEY: string;
 }
 
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = "https://bkaoishfamuishxonwhw.supabase.co";
+
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
+  SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
   {
     auth: {
